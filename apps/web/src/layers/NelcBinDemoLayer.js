024afe1fbed5e86c7055516d464de953a8e3652a
Joseph Tynan <Joseph.Tynan@Outlook.com>
2026-01-16 14:56:33 +0000

    Vendor libs: install three & shpjs; use bare imports; local heroicons SVGs; update importmap and SW precache

--- apps/web/src/layers/NelcBinDemoLayer.js
+++ apps/web/src/layers/NelcBinDemoLayer.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import * as THREE from 'three';
 import { FilteredBoundaryLayer } from './FilteredBoundaryLayer.js';
 import { latLongToVector3 } from '../globe/latLong.js';
 
